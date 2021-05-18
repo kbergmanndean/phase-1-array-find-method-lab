b939954cb0c2dd3df6e5695c24b0d56c39b8c5d5
Kaia <kbergmanndean@gmail.com>
2021-05-18 19:52:29 -0400

    commit tree 8075092a84bfd15ce8cab199825fecf5166be2bd

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+results= function(element) {
+    if (element.result==="W") {
+    return (element.result) 
+    }
+}
+
+function superbowlWin(array) {
+    const wins= array.find(results)
+    if (wins) {
+    return wins.year;
+    }
+
+}
+
